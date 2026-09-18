13b70976fd1fac7459672ae1751651141f340fa1
manish <manishsuriyal21@gmail.com>
2026-09-18 17:09:32 +0530

    fix: initialize user state from localStorage in auth store

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -2,16 +2,19 @@ import { create } from 'zustand';
 
 
 const useAuthStore = create((set) => ({
-    user: null,
-    token: null,
-    isAuthenticated: false,
+    user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null,
+    token: localStorage.getItem("token") ? localStorage.getItem("token") : null,
+    isAuthenticated: !!localStorage.getItem("token"),
 
     login: (user, token) => {
         localStorage.setItem("token", token);
+        localStorage.setItem("user", JSON.stringify(user));
+
         set({ user, token, isAuthenticated: true });
     },
     logout: () => {
         localStorage.removeItem("token");
+        localStorage.removeItem("user");
         set({ user: null, token: null, isAuthenticated: false });
     }
 }));
